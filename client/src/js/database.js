a686e2736e507201dce84f0dc7d86726c3d283e7
salzm <salmonzoor@gmail.com>
2023-05-31 10:27:15 -0400

    added to the webpack file and also some modifications to the json file. The program is not downloading, will need to ensure the program runs in heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,7 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
-  const jate = await openDB("jate", 1);
-};
+export const putDb = async (content) => console.error("putDb not implemented");
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error("getDb not implemented");
