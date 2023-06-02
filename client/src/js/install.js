5b4fef971b631abc00b1ec4c974629a7591b0604
salzm <salmonzoor@gmail.com>
2023-06-02 08:04:03 -0400

    made adjustments to the install js and src sw js file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,22 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  window.defferedPrompt = event;
+  divInstall.classlist.toggle("hidden", false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {
+  const start = window.defferedPrompt;
+  if (!start) {
+    return;
+  }
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  window.defferedPrompt = null;
+});
